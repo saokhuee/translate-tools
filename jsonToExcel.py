62c596464ca7f93682a39a56c4ac6998f251ca91
feelingBlue00 <pta230500x090800@gmail.com>
2021-03-12 15:12:59 +0700

    json to excel converter

---
+++ jsonToExcel.py
@@ -0,0 +1,41 @@
+import yaml
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+with open("vi-VN.yml", "r", encoding="utf-8") as yml_in, open("vi-VN.json", "w") as json_out:
+    yml_data = yaml.safe_load(yml_in)
+    json.dump(yml_data, json_out)
+
+with open("vi-VN.json", "r", encoding="utf-8") as json_data:
+    data = json.load(json_data)
+
+def getFullKey(obj):
+    merge_keys = {}
+
+    def flatten(item, parent_key='', sep="/"):
+        if type(item) is dict:
+            for key in item:
+                flatten(item[key], parent_key + str(key) + sep)
+
+        elif type(item) is list:
+            index = 0
+
+            for ele in item:
+                flatten(ele, parent_key + str(index) + sep)
+                index = index + 1
+        else:
+            merge_keys[parent_key[:-1]] = item
+
+    flatten(obj)
+    return merge_keys
+
+with open("temp-vi.json", "w", encoding="utf-8") as test_file:
+    json.dump(getFullKey(data), test_file, ensure_ascii=False)
+
+dataObj = pd.read_json("temp-vi.json", orient='index')
+csvFile = dataObj.to_excel("vi-VN.xlsx", encoding="utf-8") 
+
+# writer = pd.ExcelWriter("test.xlsx", encoding="utf-8")
+# csvData.to_excel(writer, index=False)
+# writer.save()
